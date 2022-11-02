ec4226c46ee0ba73e97747eccd8af34ecdbd313a
MathisPastreNelson <shiray@hotmail.fr>
2022-11-02 10:28:13 +0100

    Ajout logement.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+/* Package pour la création/gestion des routes*/
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from "./pages/Home"
 import Logement from "./pages/Logement"
 import About from "./pages/About"
 import Other from "./pages/404"
 
+/* Definition des routes de l'applications */
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                {/* Definition des routes de l'applications */}
                 <Route path="/" element={<Home />} />
                 <Route path="/logement" element={<Logement />} />
                 <Route path="/about" element={<About />} />
