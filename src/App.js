301639ddcd10624d3578d4602068edf97bfcb07c
MathisPastreNelson <shiray@hotmail.fr>
2022-11-02 16:37:03 +0100

    Ajout des images et de SASS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Logement from "./pages/Logement"
 import About from "./pages/About"
-import Other from "./pages/404"
+import NotFoundPage from "./pages/NotFound"
 
 /* Definition des routes de l'applications */
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/logement" element={<Logement />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/*" element={<Other />} />
+                <Route path="/*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
