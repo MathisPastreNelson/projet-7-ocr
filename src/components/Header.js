36b046a601ac896144b125715ebf79df45f22eb1
Mathis Pastre <shiray@hotmail.fr>
2022-11-03 01:34:05 +0100

    Définition du style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import logo from '../assets/logoSalmon.png'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-    return <div>
-        <img src={logo} alt="" />
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/about">A propos</NavLink>
+    return <div className="headerKasa">
+        <img className="logoHeader" src={logo} alt="" />
+        <div className="navLink">
+            <NavLink className="margin" to="/">Accueil</NavLink>
+            <NavLink className="margin" to="/about">A propos</NavLink>
+        </div>
     </div>
 }
 
