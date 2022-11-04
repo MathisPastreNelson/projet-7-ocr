a91d7c8a2e25ec721445581ef6ebdb3f1342ce57
Mathis Pastre <shiray@hotmail.fr>
2022-11-04 04:26:39 +0100

    Importation logements.json Tentative

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import data from '../assets/logements.json';
+console.log(data);
+
 
 const LocationList = () => {
     return <div className="locationContainer">
+        <p></p>
     </div>
 }
 
