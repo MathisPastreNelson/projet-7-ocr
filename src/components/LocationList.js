af5b267d908fd6ed1ae8f4800eff88ee71273777
MathisPastreNelson <shiray@hotmail.fr>
2022-11-07 20:30:39 +0100

    Tentative d'ajout des images

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -7,9 +7,10 @@ const Location = () => {
     return <div className="locationContainer">
         {dataList.map((data) => (
             <div className="locationCard">
-                <li className="locationlist" key={data.id}>
+                <img className="locationImg" src={data.cover} alt="Couvertures des appartements" />
+                <p className="locationlist" key={data.id}>
                     {data.title}
-                </li>
+                </p>
             </div>
         ))}
     </div>
