fd41708901a773ff37278fd4c5c85021444730c2
MathisPastreNelson <shiray@hotmail.fr>
2022-11-09 18:17:42 +0100

    Changement de couleur cards

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import NotExist from '../components/NotExist'
+import Footer from '../components/Footer';
 
 const NotFoundPage = () => {
     return (
         <div>
             <Header />
             <NotExist />
+            <Footer />
         </div>
     );
 };
