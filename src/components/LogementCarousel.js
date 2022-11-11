9f06834968b7cf40437bc934975d0ad6341548e6
Mathis Pastre <shiray@hotmail.fr>
2022-11-11 03:56:00 +0100

    Mis en place du futur composant carousel

---
+++ src/components/LogementCarousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from 'react'
+
+
+const LogementCarousel = () => {
+    return (
+        <div>Test</div>
+    )
+}
+
+export default LogementCarousel;
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import LogementCarousel from '../components/LogementCarousel'
 import Footer from '../components/Footer';
 
 const Logement = () => {
     return (
         <div>
             <Header />
-            <h1>Logements</h1>
+            <LogementCarousel />
             <Footer />
         </div>
     );
