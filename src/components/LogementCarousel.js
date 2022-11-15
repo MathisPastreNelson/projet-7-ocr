23c3e700e6446e485ad7dcef6ff110a9af3d8f58
MathisPastreNelson <shiray@hotmail.fr>
2022-11-15 18:34:07 +0100

    test affichage de donnée

--- src/components/LogementCarousel.js
+++ src/components/LogementCarousel.js
@@ -7,12 +7,12 @@ import { useParams } from "react-router-dom";
 const LogementCarousel = () => {
     const completeId = useParams();
     console.log(completeId);
+    console.log(dataList[2]);
     return (
         dataList.map((imgLocation) =>
             <div className="test" > {imgLocation.title}</div >
         )
     )
 }
-
 
 export default LogementCarousel;
