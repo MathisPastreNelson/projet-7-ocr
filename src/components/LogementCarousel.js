2b4c1178903884c6cf971b66076d95245edc5035
MathisPastreNelson <shiray@hotmail.fr>
2022-11-15 17:22:04 +0100

    tentative de recuperation de ID sur le composant logementCarousel

--- src/components/LogementCarousel.js
+++ src/components/LogementCarousel.js
@@ -1,11 +1,18 @@
 import React from "react";
+import dataList from '../assets/logements.json';
 import { useState } from 'react'
+import { useParams } from "react-router-dom";
 
 
 const LogementCarousel = () => {
+    const completeId = useParams();
+    console.log(completeId);
     return (
-        <div className="test">Emplacement du futur carousel</div>
+        dataList.map((imgLocation) =>
+            <div className="test" > {imgLocation.title}</div >
+        )
     )
 }
+
 
 export default LogementCarousel;
