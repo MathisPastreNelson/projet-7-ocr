f44b238e53283bb6e88f966405a52edbffaa4035
Mathis Pastre <shiray@hotmail.fr>
2022-11-17 05:21:06 +0100

    tentative recuperation donnée dans logementCarousel

--- src/components/LogementCarousel.js
+++ src/components/LogementCarousel.js
@@ -5,12 +5,26 @@ import { useParams } from "react-router-dom";
 
 
 const LogementCarousel = () => {
+    /* Récupération de l'ID grâce à l'URL */
     const urlParams = useParams();
+    /* Récupération du produit dans la liste complete grâce à son ID et à la méthode filter */
     const logementData = dataList.filter((data) => data.id === urlParams.id);
-    console.log(logementData);
+    console.log("Données complètes = ", logementData)
+
+    /* Récupération des data ID */
+    const logementId = logementData.map((logementData, id) => id = logementData.id)
+    console.log("ID", logementId);
+    /* Récupération des data Titres */
+    const logementTitle = logementData.map((logementData, title) => title = logementData.title)
+    console.log("Title", logementTitle);
+    /* Récupération des data Pictures */
+    const logementPictures = logementData.map((logementData, pictures) => pictures = logementData.pictures)
+    console.log("Pictures", logementPictures[0][0]);
 
     return (
-        <div className="test">test</div>
+        logementPictures.map((picture) => (
+            <img className="test" src={picture} alt="Photos des appartements" />
+        ))
     )
 }
 
