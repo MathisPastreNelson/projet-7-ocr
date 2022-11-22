cc6b7811c01347aba1ea2eb40f64e93ca2465dfa
MathisPastreNelson <shiray@hotmail.fr>
2022-11-22 23:38:16 +0100

    préparation du composant note étoile

--- src/components/LogementTitle.js
+++ src/components/LogementTitle.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import salmonStar from "../assets/starSalmon.png"
+import whiteStar from "../assets/starWhite.png"
 
 const LogementTitle = ({ logementName, logementLocation, tags, logementOwner, logementRating }) => {
-    const range = [1, 2, 3, 4, 5]
+    const range = 5
+    let salmonStarBalise = <img src={salmonStar} />
+    let whiteStarBalise = <img src={whiteStar} />
 
-    console.log(logementRating)
+    const Notation = () => {
+        while (logementRating < range) {
+            return (salmonStarBalise)
+        }
+    }
+
+    console.log("La note du logement est de ", logementRating, "/ ", range)
     return <div className="titleContainer">
         <div>
             <h2 className="titleLogement">{logementName}</h2>
@@ -17,7 +27,9 @@ const LogementTitle = ({ logementName, logementLocation, tags, logementOwner, lo
         </div>
         <div className="nameNote">
             <h4 className="logementOwner">{logementOwner.name}</h4>
-            <p className="logementRating">{logementRating}</p>
+            <div className="starContainer">
+                <Notation />
+            </div>
         </div>
     </div>
 }
