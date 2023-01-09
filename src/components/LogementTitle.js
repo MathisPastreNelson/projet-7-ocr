8f9643df2a0f01c6e0fd7d18704ed34959d792bc
MathisPastreNelson <shiray@hotmail.fr>
2023-01-09 23:55:25 +0100

    Projet Ok

--- src/components/LogementTitle.js
+++ src/components/LogementTitle.js
@@ -8,30 +8,32 @@ import whiteStar from "../assets/starWhite.png"
 const LogementTitle = ({ logementName, logementLocation, tags, logementOwner, logementRating, logementOwnerPhoto }) => {
     const rangeStars = [1, 2, 3, 4, 5];
 
-    return <div className="titleContainer">
-        <div>
-            <h2 className="titleLogement">{logementName}</h2>
-            <p className="localisationLogement">{logementLocation}</p>
-            <div className="logementTagsContainer">
-                {tags.map(tag => (
-                    <p className="logementTags" key={tag}>{tag}</p>
-                ))}
+    return (
+        <div className="titleContainer">
+            <div>
+                <h2 className="titleLogement">{logementName}</h2>
+                <p className="localisationLogement">{logementLocation}</p>
+                <div className="logementTagsContainer">
+                    {tags.map(tag => (
+                        <p className="logementTags" key={tag}>{tag}</p>
+                    ))}
+                </div>
+            </div>
+            <div className="nameNote">
+                <div className="identity">
+                    <h4 className="logementOwner">{logementOwner.name}</h4>
+                    <img className="ownerPhoto" src={logementOwnerPhoto} alt="Photo du propriétaire" />
+                </div>
+                <div className="starContainer">
+                    {rangeStars.map((star) =>
+                        logementRating < star ?
+                            <img key={star} className="whiteStar" src={whiteStar} /> :
+                            <img key={star} className="salmonStar" src={salmonStar} />
+                    )}
+                </div>
             </div>
         </div>
-        <div className="nameNote">
-            <div className="identity">
-                <h4 className="logementOwner">{logementOwner.name}</h4>
-                <img className="ownerPhoto" src={logementOwnerPhoto} alt="Photo du propriétaire" />
-            </div>
-            <div className="starContainer">
-                {rangeStars.map((star) =>
-                    logementRating < star ?
-                        <img key={star} className="whiteStar" src={whiteStar} /> :
-                        <img key={star} className="salmonStar" src={salmonStar} />
-                )}
-            </div>
-        </div>
-    </div>
+    )
 }
 
 export default LogementTitle;
