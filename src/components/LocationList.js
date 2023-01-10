1c46c0dd57e0c357c6f0cffeb0c7d54dd0967398
MathisPastreNelson <shiray@hotmail.fr>
2023-01-10 15:20:45 +0100

    Project Ok

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -5,17 +5,18 @@ import { NavLink } from 'react-router-dom'
 import dataList from '../assets/logements.json';
 
 const Location = () => {
-    return <div className="locationContainer">
-        {/* Mon .map() me permet d'aller chercher toutes les données */}
-        {dataList.map((data) => (
-            <NavLink className="locationCard" key={data.id} to={"/logement/" + data.id}>
-                <img className="locationImg" src={data.cover} alt={data.title} />
-                <p className="locationTitle" >
-                    {data.title}
-                </p>
-            </NavLink>
-        ))}
-    </div>
+    return (
+        <div className="locationContainer">
+            {/* Mon .map() me permet d'aller chercher toutes les données */}
+            {dataList.map((data) => (
+                <NavLink className="locationCard" key={data.id} to={"/logement/" + data.id}>
+                    <img className="locationImg" src={data.cover} alt={data.title} />
+                    <p className="locationTitle" >
+                        {data.title}
+                    </p>
+                </NavLink>
+            ))}
+        </div>)
 }
 
 export default Location;
